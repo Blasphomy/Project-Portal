5bf441c3f545b3c137170e74dad569578adc88a7
Adi Aggarwal <josephjonas9999@gmail.com>
2025-11-04 14:59:43 +0530

    Initial commit

---
+++ src/test/java/com/project/Project_Portal/ProjectPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Project_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
